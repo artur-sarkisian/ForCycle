ee4513ab8643fd3bad9f5204f80d43601bb852cc
artursarkisyan <sai_7@mail.ru>
2023-03-08 18:55:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= value; i++) {
+            for (int j = 1; j <= value; j++) {
+                if (i * j == value) System.out.println(i + "*" + j);
+            }
+        }
+    }
+}
